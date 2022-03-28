41317b956c54f3a1e11c3ec1046be33ba636fa89
Greg Pataky <greg@pata.ky>
2022-03-27 17:32:53 -0700

    Added guard for Debug implementation to allow for no std and loom.

--- src/lib.rs
+++ src/lib.rs
@@ -252,6 +252,7 @@ unsafe impl<T, F, P> Send for AtomicPtr<T, F, P> {}
 // (and thus may take ownership of one) ensure that `T: Send`.
 unsafe impl<T, F, P> Sync for AtomicPtr<T, F, P> {}
 
+#[cfg(all(not(loom), feature = "std"))]
 impl<T, F, P> std::fmt::Debug for AtomicPtr<T, F, P> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.fmt(f)
